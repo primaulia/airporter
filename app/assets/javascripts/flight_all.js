831b7cbed0fdf9f33de665f29ffa0ca0b28cb046
Prima Aulia Gusta <primaulia@gmail.com>
2017-05-05 13:34:56 +0800

    setup redis to heroku

--- app/assets/javascripts/flight_all.js
+++ app/assets/javascripts/flight_all.js
@@ -1,7 +1,7 @@
 $(document).on('ready page:load', function () {
   if($('#all-flight').length) {
     console.log('run specific js file for all flight page')
-    gon.flights.forEach((data) => {
+    gon.flights.forEach(function(data) {
       console.log(JSON.stringify(data))
     })
   }
