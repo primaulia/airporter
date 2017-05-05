f029527f6c07ab062646b43c452d073f96392879
Prima Aulia Gusta <primaulia@gmail.com>
2017-05-05 16:26:46 +0800

    stable users with following/follower r/s

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ApplicationRecord
   has_many :nominees, class_name: 'User', foreign_key: 'nominator_id'
   belongs_to :nominator, class_name: 'User', optional: true
 
+  has_many :active_followings, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :passive_followings, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+
+  has_many :followings, through: :active_followings, source: :followed
+  has_many :followers, through: :passive_followings, source: :follower
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
